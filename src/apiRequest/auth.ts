a48b3cd0a4b672a622146cfbcbf0227b617b31c3
Dematto04 <mlong200004@gmail.com>
2024-10-28 15:22:36 +0700

    setup http request and build login ui

---
+++ src/apiRequest/auth.ts
@@ -0,0 +1,15 @@
+import http from "@/lib/http";
+import { LoginBodyType, LoginResType } from "@/schemaValidations/auth.schema";
+
+const authApiRequest = {
+  sLogin: (body: LoginBodyType) => {
+    return http.post<LoginResType>("/auth/login", body);
+  },
+  login: (body: LoginBodyType) => {
+    return http.post<LoginResType>("/api/auth/login", body, {
+      baseUrl: "",
+    });
+  },
+};
+
+export default authApiRequest
