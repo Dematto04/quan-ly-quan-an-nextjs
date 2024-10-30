4a225b3e43df48cbac8dce9e64f45570a437bf33
Dematto04 <mlong200004@gmail.com>
2024-10-30 16:49:56 +0700

    update profile

--- src/app/manage/dropdown-avatar.tsx
+++ src/app/manage/dropdown-avatar.tsx
@@ -10,25 +10,45 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
+import { useLogoutMutation } from '@/queries/useAuth'
+import { toast } from '@/hooks/use-toast'
+import { handleErrorApi } from '@/lib/utils'
+import { useRouter } from 'next/navigation'
+import { useAccountMe } from '@/queries/useAccount'
 
-const account = {
-  name: 'Nguyễn Văn A',
-  avatar: 'https://i.pravatar.cc/150'
-}
 
 export default function DropdownAvatar() {
+  const logoutMutation = useLogoutMutation()
+  const {data} = useAccountMe()
+  const account = data?.payload.data
+  const router = useRouter()
+
+  const logout =  async ()=> {
+    if(logoutMutation.isPending) return 
+    
+    try {
+      const result = await logoutMutation.mutateAsync()
+      toast({
+        description: 'Đăng xuất thành công'
+      })
+      router.replace('/')
+    } catch (error) {
+      handleErrorApi({error})
+    }
+
+  }
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant='outline' size='icon' className='overflow-hidden rounded-full'>
           <Avatar>
-            <AvatarImage src={account.avatar ?? undefined} alt={account.name} />
-            <AvatarFallback>{account.name.slice(0, 2).toUpperCase()}</AvatarFallback>
+            <AvatarImage src={account?.avatar || undefined} alt={account?.name} />
+            <AvatarFallback>{account?.name.slice(0, 2).toUpperCase()}</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align='end'>
-        <DropdownMenuLabel>{account.name}</DropdownMenuLabel>
+        <DropdownMenuLabel>{account?.name}</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem asChild>
           <Link href={'/manage/setting'} className='cursor-pointer'>
@@ -37,7 +57,7 @@ export default function DropdownAvatar() {
         </DropdownMenuItem>
         <DropdownMenuItem>Hỗ trợ</DropdownMenuItem>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>Đăng xuất</DropdownMenuItem>
+        <DropdownMenuItem onClick={logout}>Đăng xuất</DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   )
---
+++ src/queries/useAccount.tsx
@@ -0,0 +1,16 @@
+import accountApiRequest from "@/apiRequest/account"
+import { AccountResType } from "@/schemaValidations/account.schema"
+import { useMutation, useQuery } from "@tanstack/react-query"
+
+export const useAccountMe = () => {
+    return useQuery({
+        queryKey: ['account-me'],
+        queryFn: () => accountApiRequest.me()
+    })
+}
+export const useUpdateMeMutation = () => {
+    return useMutation({
+        mutationFn: accountApiRequest.updateMe
+    })
+
+}
